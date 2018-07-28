b11071ca2589f415368fdb913a04539d5379a58f
Amitai Getzler <amitai333@gmail.com>
2018-07-28 13:27:40 +0300

    update

--- eventer.py
+++ eventer.py
@@ -1,6 +1,6 @@
 import codecs
 import pandas as pd
-#import urllib2
+from variables import *
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
@@ -8,25 +8,6 @@ import requests
 import string
 import re
 import sys
-
-# Variables:
-haifa = "110619208966868"
-tel_aviv = "106371992735156"
-usr = "amitai333@gmail.com"
-pwd = "Amitai321"
-url = 'https://www.facebook.com/events/discovery/?suggestion_token={"time":"today","city":"'
-url += haifa
-url += '"}&acontext={"source":2,"source_dashboard_filter":"discovery","action_history":"[{\"surface\":\"dashboard\",\"mechanism\":\"dashboard_home_discovery_filter\"},{\"surface\":\"discover_filter_list\",\"mechanism\":\"surface\",\"extra_data\":{\"dashboard_filter\":\"discovery\"}}]","has_source":true}'
-getID = """ return document.getElementsByClassName("_7ty")[0].href """
-SCROLL_PAUSE_TIME = 1.5
-
-getName = """ return document.getElementById("pageTitle").text """
-getStartingTime = """ return document.getElementsByClassName("_164e")[0].nextSibling.nextSibling.innerText """
-getEndingTime = """ return document.getElementsByClassName("_164e")[0].nextSibling.nextSibling.nextSibling.nextSibling.innerText """
-getAddr = """ return document.getElementsByClassName("_5xhp fsm fwn fcg")[1].innerText """
-getPlace = """ return document.getElementsByClassName("_5xhk")[1].text """
-getImage = """ return document.getElementsByClassName("uiScaledImageContainer _3ojl")[0].childNodes[0].src """
-getImage2 = """ return document.getElementsByClassName("_m54 _1jto _3htz hidden_elem")[0].childNodes[0].src """
 
 chrome_options = webdriver.ChromeOptions()
 prefs = {"profile.default_content_setting_values.notifications" : 2}
@@ -43,6 +24,7 @@ elem = driver.find_element_by_id("pass")
 elem.send_keys(pwd)
 elem.send_keys(Keys.RETURN)
 
+'''
 while (True):
     try:
         driver.execute_script(getID)
@@ -50,19 +32,19 @@ while (True):
     except:
         print("Waiting")
         time.sleep(0.5)
-
+'''
 # Get scroll height
-last_height = driver.execute_script("return document.body.scrollHeight")
+last_height = driver.execute_script(last)
 
 while True:
     # Scroll down to bottom
     try:
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        driver.execute_script(scroll)
         # Wait to load page
         time.sleep(SCROLL_PAUSE_TIME)
 
         # Calculate new scroll height and compare with last scroll height
-        new_height = driver.execute_script("return document.body.scrollHeight")
+        new_height = driver.execute_script(last)
         if new_height == last_height:
             break
         last_height = new_height
@@ -147,58 +129,9 @@ file.close()
 driver.close()
 
 df = pd.read_csv("csvfile.csv",encoding='utf-8')
-#df.style.format(percent)applymap(color_negative_red, subset=['col1', 'col2']).set_properties(**{'font-size': '9pt', 'font-family': 'Calibri'}).bar(subset=['col4', 'col5'], color='lightblue').render()
 pd.set_option('display.max_colwidth', -1)
 HTML_file = df.to_html(classes='utf8')
-HTML_file += """ <style>
-html,
-body {
-	height: 100%;
-}
-
-body {
-	margin: 0;
-	background: linear-gradient(45deg, #49a09d, #5f2c82);
-	font-family: sans-serif;
-	font-weight: 100;
-  margin: 0;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-}
-
-.container {
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-}
-
-table {
-	width: 100%;
-    height: 100%;
-	border-collapse: collapse;
-	box-shadow: 0 0 20px rgba(0,0,0,0.1);
-}
-
-th,
-td {
-	padding: 15px;
-	background-color: rgba(255,255,255,0.2);
-	color: #fff;
-  word-wrap: break-word;
-}
-
-th {
-	text-align: left;
-}
-
-thead {
-	th {
-		background-color: #55608f;
-	}
-}
-</style>
-"""
+HTML_file += styling
 
 file = codecs.open('utf8html.html','w','utf-8')
 file.write('<meta http-equiv=\"Content-Type\" content=\"text/html;charset=UTF-8\">')
---
+++ variables.py
@@ -0,0 +1,71 @@
+haifa = "110619208966868"
+tel_aviv = "106371992735156"
+usr = "amitai333@gmail.com"
+pwd = "Amitai321"
+url1 = 'https://www.facebook.com/events/discovery/?suggestion_token={"time":"today","city":"'
+url2 = url1 + haifa
+url = url2 + '"}&acontext={"source":2,"source_dashboard_filter":"discovery","action_history":"[{\"surface\":\"dashboard\",\"mechanism\":\"dashboard_home_discovery_filter\"},{\"surface\":\"discover_filter_list\",\"mechanism\":\"surface\",\"extra_data\":{\"dashboard_filter\":\"discovery\"}}]","has_source":true}'
+
+SCROLL_PAUSE_TIME = 1.5
+prefs = {"profile.default_content_setting_values.notifications" : 2}
+
+getName = """ return document.getElementById("pageTitle").text """
+getStartingTime = """ return document.getElementsByClassName("_164e")[0].nextSibling.nextSibling.innerText """
+getEndingTime = """ return document.getElementsByClassName("_164e")[0].nextSibling.nextSibling.nextSibling.nextSibling.innerText """
+getAddr = """ return document.getElementsByClassName("_5xhp fsm fwn fcg")[1].innerText """
+getPlace = """ return document.getElementsByClassName("_5xhk")[1].text """
+getImage = """ return document.getElementsByClassName("uiScaledImageContainer _3ojl")[0].childNodes[0].src """
+getImage2 = """ return document.getElementsByClassName("_m54 _1jto _3htz hidden_elem")[0].childNodes[0].src """
+
+getID = """ return document.getElementsByClassName("_7ty")[0].href """
+last = """ return document.body.scrollHeight """
+scroll = """ window.scrollTo(0, document.body.scrollHeight); """
+styling = """ <style>
+html,
+body {
+	height: 100%;
+}
+
+body {
+	margin: 0;
+	background: linear-gradient(45deg, #49a09d, #5f2c82);
+	font-family: sans-serif;
+	font-weight: 100;
+  margin: 0;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
+
+.container {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+}
+
+table {
+	width: 100%;
+    height: 100%;
+	border-collapse: collapse;
+	box-shadow: 0 0 20px rgba(0,0,0,0.1);
+}
+
+th,
+td {
+	padding: 15px;
+	background-color: rgba(255,255,255,0.2);
+	color: #fff;
+  word-wrap: break-word;
+}
+
+th {
+	text-align: left;
+}
+
+thead {
+	th {
+		background-color: #55608f;
+	}
+}
+</style>
+"""
