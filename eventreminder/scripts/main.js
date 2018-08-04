da3d042addc3ebe71d20609174c75bca950daf63
ShacharHarshuv <shachar.harshuv@gmail.com>
2018-08-04 14:56:56 +0300

    Complete desktop event-card look

--- eventreminder/scripts/main.js
+++ eventreminder/scripts/main.js
@@ -6,9 +6,19 @@ $(function() {
         //Add expand functionality
         $expand_btn = $(this).find(".btn-expander");
         $expand_content = $(this).find(".description");
-
         $expand_btn.on("click", function(){
-            $expand_content.slideToggle();
+            $expand_content.slideToggle(function(){
+                $expand_btn.toggleClass("collapsed expanded");
+            });
+        });
+
+        //Add Go functionality
+        console.log($(this));
+        $go_btn = $(this).find(".btn-go");
+        console.log($go_btn);
+        $go_btn.on("click", function(){
+            $(this).toggleClass("btn-going btn-not-going");
+            console.log("clicked");
         });
     });
 });
